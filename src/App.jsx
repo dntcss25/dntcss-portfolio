e40ef01eb3dcefc3877f531b171ba81510e6cbd9
Deniel Angelo Bare <denielangelobare@gmail.com>
2024-11-10 22:10:53 +0800

    update navheader

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,14 @@
-import './App.css'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Navheader from "./components/Navheader"
 
-// Create simple page components
-function Home() {
-  return <h2>Home Page</h2>;
-}
-
-function About() {
-  return <h2>About Page</h2>;
-}
-
-function App() {
+const App = () => {
   return (
-    <Router>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
-  );
+    <div>
+      <Navheader />
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
 }
 
 export default App
