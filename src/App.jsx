36628d54747ca842236e8d6ab63b1e49d4c76e81
Deniel Angelo Bare <denielangelobare@gmail.com>
2024-11-10 21:37:01 +0800

    update react file

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+// Create simple page components
+function Home() {
+  return <h2>Home Page</h2>;
+}
+
+function About() {
+  return <h2>About Page</h2>;
+}
+
+function App() {
+  return (
+    <Router>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App
